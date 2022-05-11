4893fadfb1c3e9452214f8c8c166178c5a420da3
Jakub Binczyk <jbinczyk@users.noreply.github.com>
2022-05-11 19:13:49 +0200

    Revert "Tabu eucl poczatek"
    This reverts commit c41601f329741f06cf556bc49d7ec572f5b9cad1.

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 COMPILER = g++
 
 MORPHOLOGICA_PATH = /home/kacper/semestr6/metaheurystyczne/l1/build/morphologica
-#MORPHOLOGICA_PATH = /home/jakub/University/morphologica-main
 
 SHARED_LIBS = /usr/lib/x86_64-linux-gnu/libglfw.so.3.3 /usr/lib/x86_64-linux-gnu/libOpenGL.so /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib/x86_64-linux-gnu/libGLX.so /usr/lib/x86_64-linux-gnu/libhdf5_cpp.so.103 /usr/lib/x86_64-linux-gnu/libhdf5_serial.so
 
@@ -35,4 +34,4 @@ experiments:
 	$(COMPILER) $(REQUIRED_INCLUDES) $(COMPILER_REQ_FLAGS) $(COMPILER_SETTINGS) -o testExperiments testExperiments.cpp $(SHARED_LIBS)
 
 clean:
-	rm -f testVisual testLoad testSave testExperiments testTabu tabuExperiments
+	rm -f testVisual testLoad testSave testExperiments testTabu
--- testTabu.cpp
+++ testTabu.cpp
@@ -1,10 +1,8 @@
 #include "TabuList.hpp"
 #include "EuclideanTSPInstance.hpp"
-#include "MatrixTSPInstance.hpp"
 
 int main()
 {
-    std::cout << "EUCL" << std::endl;
     EuclideanTSPInstance euclInstance;
     euclInstance.randomInstance(clock(), 700);
     euclInstance.solveTabuSearch(7, CLOCKS_PER_SEC * 30, true, false, true, &EuclideanTSPInstance::solve2Opt, &EuclideanTSPInstance::symmetricInvert, 
@@ -21,24 +19,6 @@ int main()
 
 
     euclInstance.deleteInstance();
-
-    std::cout << "MATRIX" << std::endl;
-    MatrixTSPInstance matrixTspInstance;
-    matrixTspInstance.randomInstance(clock(), 700, true);
-    matrixTspInstance.solveTabuSearch(7, CLOCKS_PER_SEC * 30, true, false, true, true, &MatrixTSPInstance::solve2Opt, &EuclideanTSPInstance::symmetricInvert,
-                                 &EuclideanTSPInstance::symmetricInvertNeighboorhood, &EuclideanTSPInstance::invertAcceleratedMeasurement);
-
-    matrixTspInstance.randomInstance(clock(), 700, true);
-    matrixTspInstance.solveTabuSearch(7, CLOCKS_PER_SEC * 30, true, false, false, true, &MatrixTSPInstance::solve2Opt, &EuclideanTSPInstance::symmetricSwap,
-                                 &EuclideanTSPInstance::symmetricSwapNeighboorhood, &EuclideanTSPInstance::swapAcceleratedMeasurement);
-
-
-    matrixTspInstance.randomInstance(clock(), 700, false);
-    matrixTspInstance.solveTabuSearch(7, CLOCKS_PER_SEC * 30, true, false, true, false, &MatrixTSPInstance::solve2Opt, &EuclideanTSPInstance::symmetricInsert,
-                                 &EuclideanTSPInstance::symmetricInsertNeighboorhood, &EuclideanTSPInstance::insertAcceleratedMeasurement);
-
-
-    matrixTspInstance.deleteInstance();
 
     return 0;
 }
